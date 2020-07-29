84d1f13143a415f7ea6f94d7c91b3c0ece15492e
your name here <johnobrien8642@gmail.com>
2020-07-29 19:33:18 -0400

    Changed class name to AllWordChains, words variable reads in dictionary.txt, @dictionary creates Array of var words

--- all_word_chains.rb
+++ all_word_chains.rb
@@ -1,6 +1,11 @@
 
 
-class WordChainsRecursive
+class AllWordChains
+
+    def initialize 
+        words = File.readlines("dictionary.txt").map(&:chomp)
+        @dictionary = Array.new(words)
+    end
 
     
 
